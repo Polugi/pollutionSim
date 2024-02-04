1b47a52f063a6d6182f6f7ad49326a8fcf3ff8a0
sachitt <sachittksrivastava@gmail.com>
2024-02-04 23:16:18 +0800

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,7 @@
-
-
 public class Main {
     public static void main(String[] args) {
 
-//        String file = "src\\indiaHealthStatus.csv";
-
         GameFrame gp = new GameFrame();
-
-
-
-
-
 
     }
 }
